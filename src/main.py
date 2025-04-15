8ac30d3ac4e3ef55b8a61c14cf9e359f0333bc27
Lijie Ding <dingl1@ornl.gov>
2025-04-15 00:00:05 -0400

    add multi run argv

--- src/main.py
+++ src/main.py
@@ -2,12 +2,12 @@ from simulation_engine import SimulationEngine
 from analyze import *
 import sys
 
-def main(rounds=10, p=1.0):
+def main(rounds=10, p=1.0, run=0):
     # Specify the path for storing voting results
     folder = "../data/data_pool"
     # Run the simulation
     topic = "pineapple on pizza"  # Example topic for discussion
-    finfo = f"pieneapple_rounds_{rounds:.0f}_p_{p:.1f}"
+    finfo = f"pieneapple_rounds_{rounds:.0f}_p_{p:.1f}_run_{run}"
     engine = SimulationEngine("config/agents.json", topic, rounds, p)
     engine.run_simulation(folder, finfo)
 
@@ -20,16 +20,17 @@ def main(rounds=10, p=1.0):
 
 if __name__ == "__main__":
     # Check if the user provided command-line arguments for rounds and p
-    if len(sys.argv) == 3:
+    if len(sys.argv) == 4:
         try:
             rounds = int(sys.argv[1])
             p = float(sys.argv[2])
+            run = int(sys.argv[3])
         except ValueError:
             print("Invalid input. Please provide an integer for rounds and a float for p (e.g., python3 main 10 1.0).")
             sys.exit(1)
-        print(f"Using provided values: rounds={rounds}, p={p}")
+        print(f"Using provided values: rounds={rounds}, p={p}, run={run}")
         main(rounds, p)
     else:
-        print("Using default values: rounds=10, p=1.0")
+        print("Using default values: rounds=10, p=1.0, run=0")
         # If no arguments are provided, use the default values.
         main()
